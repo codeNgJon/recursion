9e025c3e01618c46ea4b7c30e94101ebe08dbeb1
codengjon <1jonathan.ng@gmail.com>
2014-07-02 23:06:07 -0700

    completed re-implementation of getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,7 +3,37 @@
 //   return document.getElementsByClassName(className);
 // };
 
-// But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+	var body = document.body; 
+	var children = body.childNodes; // all direct nodes of body in array
+	var elements = [];
+
+	//recursive function
+	var checkChild = function(array){
+	  for(var i=0; i<array.length; i++){
+	    var node = array[i];
+	    var list = node.classList;
+	      for(var key in list){
+		    if(list[key] === className){
+		      elements.push(node)
+		    }
+		  }
+	    if(node.childNodes){
+	  	  checkChild(node.childNodes);
+	    }
+	  }
+    }
+    //check body element first then send everything else into the recursive function
+	var arr = body.classList
+	for(var i=0; i<arr.length; i++){
+		if(arr[i] === className){
+			elements.push(body);
+		}
+	}
+	//use the recursive function for all the child nodes of body and rest of DOM 
+	var nodes = body.childNodes
+	if(nodes.length > 0){
+	  checkChild(nodes);
+	}
+	return elements;
 };
